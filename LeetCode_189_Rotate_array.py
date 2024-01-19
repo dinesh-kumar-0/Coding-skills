00a37391e8676382ef37bb33c173e11953dc52ab
dinesh-kumar-0 <134045482+dinesh-kumar-0@users.noreply.github.com>
2024-01-20 00:24:44 +0530

    Update LeetCode_189_Rotate_array.py
    refer this link:
    https://leetcode.com/problems/rotate-array/description/

--- LeetCode_189_Rotate_array.py
+++ LeetCode_189_Rotate_array.py
@@ -1,4 +1,4 @@
-# rotate the array k times with O(1) time complexity
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k = k%(len(nums))

dc521e319a7848ff02e6166e633d234cefec07af
dinesh-kumar-0 <134045482+dinesh-kumar-0@users.noreply.github.com>
2024-01-20 09:12:22 +0530

    Create LeetCode_1_TwoSum.py
    refer this link:
    https://leetcode.com/problems/two-sum/description/

---
+++ LeetCode_1_TwoSum.py
@@ -0,0 +1,18 @@
+Problem: Two Sum 
+
+Solution : 
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    
+        for(int i = 0; i<nums.length; i++){
+            for(int j =i+1; j<nums.length; j++){
+                if(nums[i] + nums [j] == target) {
+                    int ts[] = {i, j};
+                    return ts;
+                }
+            }
+        }
+        return null;
+    }
+}
